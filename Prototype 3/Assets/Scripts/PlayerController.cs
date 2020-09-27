8ede75ee9a1ffcbc538728bc425d1ded41040343
Gregory Blevins <gblevins@mail.bradley.edu>
2020-09-27 18:09:42 -0500

    Prototype 3
    Here's all of Prototype 3

---
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Gregory Blevins
+ * Prototype 3
+ * Player Controls, effects, and failure condition script
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Animator playerAnimator;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+
+    private AudioSource playerAudio;
+
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+
+    public float jumpForce;
+    public ForceMode jumpForceMode;
+    public float gravityModifier;
+
+    public bool isOnGround = true;
+    public bool gameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        playerAnimator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+
+        jumpForceMode = ForceMode.Impulse;
+
+        if (Physics.gravity.y > -10)
+        {
+            Physics.gravity *= gravityModifier;
+        }
+        //Physics.gravity *= gravityModifier;
+
+        playerAnimator.SetFloat("Speed_f", 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            playerAnimator.SetTrigger("Jump_trig");
+            rb.AddForce(Vector3.up * jumpForce, jumpForceMode);
+
+            dirtParticle.Stop();
+
+            isOnGround = false;
+
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }   
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            dirtParticle.Play();
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over!");
+            gameOver = true;
+
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", 1);
+
+            explosionParticle.Play();
+
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+
+            dirtParticle.Stop();
+        }
+    }
+}
---
+++ Prototype 3/Assets/Scripts/TriggerZoneAddScore.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Gregory Blevins
+ * Prototype 3
+ * Increments Score for successful jumps
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZoneAddScore : MonoBehaviour
+{
+    private UIManager uIManager;
+
+    private bool triggered = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uIManager = GameObject.FindObjectOfType<UIManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !triggered)
+        {
+            uIManager.score++;
+            triggered = true;
+        }
+    }
+}
