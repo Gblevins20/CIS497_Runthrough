1dd8f476dd643474a08c19c7e6e725a486faee0c
Gregory Blevins <gblevins@mail.bradley.edu>
2020-12-14 00:04:32 -0600

    commit
    figured it out

---
+++ Assignment 9 NavMesh/Assignment 9 NavMesh/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Gregory Blevins
+ * Assignment 9
+ * Handles moving a NavMeshAgent to a specific location
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    public Transform goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
